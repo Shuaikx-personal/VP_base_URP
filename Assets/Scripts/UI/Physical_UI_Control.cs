688959137c0c3002a2fc6d4de01c466f949436d9
Shuaikx <shuaikx828@gmail.com>
2025-07-31 21:16:17 +0800

    ready fot integrate URP.

---
+++ Assets/Scripts/UI/Physical_UI_Control.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class Physical_UI_Control : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void DoQuit()
+    {
+        Debug.Log("Quit");
+        Application.Quit();
+    }
+}
